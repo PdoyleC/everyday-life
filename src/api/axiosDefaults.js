b64a0696552e476b3ec7f27d6793895115c652f0
PdoyleC <>
2024-01-25 16:11:22 +0000

    NavBar loggedIn/loggedOut

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://live-life-api-now-f6f9c42b2398.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
