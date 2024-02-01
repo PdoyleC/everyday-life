708e6d4cc8430b8929fd9d956adecdcdce93dca2
PdoyleC <>
2024-02-01 14:07:35 +0000

    changed hover colours

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,7 +37,7 @@ const NavBar = () => {
       activeClassName={styles.Active}
       to="/posts/create"
     >
-      <i className="far fa-plus-square"></i>Add post
+      <i className="far fa-plus-square"></i>Add New Post
     </NavLink>
   );
   const loggedInIcons = (
